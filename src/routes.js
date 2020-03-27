6fa8604737ad955796a09a26c1e88f5d017f071b
Fabio Maciel <macielfsouza@gmail.com>
2020-03-27 15:50:49 -0300

    add validation to some routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
+
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
@@ -9,12 +11,34 @@ const routes = express.Router()
 routes.post('/session', SessionController.create)
 
 routes.get('/ongs', OngController.list);
-routes.post('/ongs', OngController.create);
 
-routes.get('/incidents', IncidentController.list);
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required(),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+}), OngController.create);
+
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number()
+  })
+}) ,IncidentController.list);
+
 routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object({
+    id: Joi.number().required()
+  })
+}),IncidentController.delete);
 
-routes.get('/profile', ProfileController.list)
+routes.get('/profile',celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown()
+}), ProfileController.list)
 
 module.exports = routes;
